c23ceb87aa4389d7d8be9f17e319a2a72c51e6f6
Sherif-Makii <sherif.makii@gmail.com>
2025-12-21 14:47:01 +0200

    This is a demo for ecom automation test

---
+++ readme.txt
@@ -0,0 +1,3 @@
+Hello Dear
+
+This is an Example for Automation test using Selenuim created by ENG : Sherif Makii (sherif.makii@gmail.com)
---
+++ src/test/java/org_examples/StepDef/Hooks.java
@@ -0,0 +1,38 @@
+package org_examples.StepDef;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.asserts.SoftAssert;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Hooks {
+    public static WebDriver driver;
+    static SoftAssert soft = new SoftAssert();
+
+    @Before
+    public void open_browser() {
+        //C:\Program Files (x86)\chromedriver.exe
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+       //driver = new EdgeDriver();
+
+        // 3- configuration to your driver
+        driver.manage().window().maximize();
+       //driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
+    @After
+    public void quit_from_browser() {
+
+       // driver.quit();
+    }
+
+
+}
